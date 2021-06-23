df1dae1bba979903dd9975090931f499412f8caa
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2021-06-22 19:22:55 -0500

    adding test for readme.md

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 const assert = require('assert');
 const path = require('path');
-const { type } = require("os");
+const {type} = require("os");
+const {testing} = require("googleapis/build/src/apis/testing");
 
 const fieldIsA = (name, field, type) => {
     assert.strictEqual(typeof (field), type, `❌ ${name} isn\'t a ${type}`)
@@ -14,65 +15,75 @@ const fieldIsAnArray = (name, field) => {
 }
 const isAString = (field) => assert.strictEqual(typeof (field), 'string')
 
-const items = fs.readdirSync('recipes');
-items.forEach(item => {
-    console.log(`📑 ${item}`);
-    console.group()
-    try {
-        const recipe = JSON.parse(fs.readFileSync(`recipes/${item}`));
-        const {
-            title, servings, time, rating, slug, notes,
-            source, image, instructions, ingredients,
-            createdDate, ...unused
-        } = recipe;
 
-        assert.ok(!Object.keys(unused).length, '❌ extra fields found in JSON');
-        console.log('✅ exact number of fields')
+const test = () => {
+    console.log('Project:')
+    assert.ok(fs.existsSync('README.md'), '❌ no readme file found')
+    console.log('✅ README.md exists!')
 
-        fieldIsA('title', title, 'string');
-        fieldIsA('servings', servings, 'number');
-        fieldIsA('rating', rating, 'number');
-        fieldIsA('slug', slug, 'string');
-        fieldIsA('source', source, 'string');
+    console.log('Recipes:')
+    const items = fs.readdirSync('recipes');
+    items.forEach(item => {
+        console.log(`📑 ${item}`);
+        console.group()
+        try {
+            const recipe = JSON.parse(fs.readFileSync(`recipes/${item}`));
+            const {
+                title, servings, time, rating, slug, notes,
+                source, image, instructions, ingredients,
+                createdDate, ...unused
+            } = recipe;
 
-        if (createdDate) {
-            assert.ok(!isNaN(new Date(createdDate).getTime()), '❌ created date is not a date')
-            console.log('✅ created date');
+            assert.ok(!Object.keys(unused).length, '❌ extra fields found in JSON');
+            console.log('✅ exact number of fields')
+
+            fieldIsA('title', title, 'string');
+            fieldIsA('servings', servings, 'number');
+            fieldIsA('rating', rating, 'number');
+            fieldIsA('slug', slug, 'string');
+            fieldIsA('source', source, 'string');
+
+            if (createdDate) {
+                assert.ok(!isNaN(new Date(createdDate).getTime()), '❌ created date is not a date')
+                console.log('✅ created date');
+            }
+
+            fieldIsAnArray('instructions', instructions)
+            fieldIsAnArray('notes', instructions)
+
+            assert.ok(Array.isArray(ingredients), '❌ ingredients is not an array');
+            ingredients.forEach(ingredient => {
+                const {category, items, ...extra} = ingredient;
+
+                assert.ok(!Object.keys(extra).length, '❌ extra fields underneath ingredients');
+                assert.strictEqual(typeof (category), 'string', '❌ category is not a string');
+                assert.ok(Array.isArray(items), '❌ items is not an array');
+            })
+            console.log('✅ ingredients')
+
+            assert.ok(Array.isArray(time), '❌ time is not an array');
+            time.forEach(time => {
+                const {label, units, ...extra} = time;
+
+                assert.ok(!Object.keys(extra).length, '❌ extra fields underneath time');
+                assert.strictEqual(typeof label, 'string', '❌ label is not a string');
+                assert.strictEqual(typeof units, 'string', '❌ units is not a string')
+            });
+            console.log('✅ time')
+
+            if (image) {
+                const imagePath = path.normalize(`recipes/${image}`);
+                assert(fs.existsSync(imagePath), '❌ image path is not correct');
+                console.log('✅ image')
+            }
+            console.log('Looks 👌')
+        } catch (err) {
+            console.error(err);
+            assert.fail(`❌ failed: ${item}`)
+        } finally {
+            console.groupEnd()
         }
+    });
+}
 
-        fieldIsAnArray('instructions', instructions)
-        fieldIsAnArray('notes', instructions)
-
-        assert.ok(Array.isArray(ingredients), '❌ ingredients is not an array');
-        ingredients.forEach(ingredient => {
-            const { category, items, ...extra } = ingredient;
-
-            assert.ok(!Object.keys(extra).length, '❌ extra fields underneath ingredients');
-            assert.strictEqual(typeof (category), 'string', '❌ category is not a string');
-            assert.ok(Array.isArray(items), '❌ items is not an array');
-        })
-        console.log('✅ ingredients')
-
-        assert.ok(Array.isArray(time), '❌ time is not an array');
-        time.forEach(time => {
-            const { label, units, ...extra } = time;
-
-            assert.ok(!Object.keys(extra).length, '❌ extra fields underneath time');
-            assert.strictEqual(typeof label, 'string', '❌ label is not a string');
-            assert.strictEqual(typeof units, 'string', '❌ units is not a string')
-        });
-        console.log('✅ time')
-
-        if (image) {
-            const imagePath = path.normalize(`recipes/${image}`);
-            assert(fs.existsSync(imagePath), '❌ image path is not correct');
-            console.log('✅ image')
-        }
-        console.log('Looks 👌')
-    } catch (err) {
-        console.error(err);
-        assert.fail(`❌ failed: ${item}`)
-    } finally {
-        console.groupEnd()
-    }
-});
+test();
