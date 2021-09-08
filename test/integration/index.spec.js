08ad9a8da1762c087586c2031056c616d25adc0f
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2021-09-07 19:52:35 -0500

    adding test for preventing orphaned images + removing orphaned images

--- test/integration/index.spec.js
+++ test/integration/index.spec.js
@@ -2,10 +2,11 @@ const {expect} = require('@jest/globals');
 const fs = require('fs');
 
 describe('Integration', () => {
-    let files;
+    let files, recipes;
 
     beforeEach(() => {
         files = fs.readdirSync('lib');
+        recipes = files.map(file => JSON.parse(fs.readFileSync(`lib/${file}`)));
     });
 
     test('README.md should exist', () => {
@@ -13,10 +14,17 @@ describe('Integration', () => {
     });
 
     test('scraped recipes should match schema', () => {
-        files.forEach(file => {
-            const recipe = JSON.parse(fs.readFileSync(`lib/${file}`));
-
+        recipes.forEach(recipe => {
             expect(recipe).toBeARecipe();
+        })
+    });
+
+    test('should not have orphaned images', () => {
+        const recipesImages = recipes.map(recipe => recipe.image).filter(Boolean).map(imagePath => imagePath.split('/').pop());
+        const imageFiles = fs.readdirSync('images');
+
+        imageFiles.forEach(file => {
+            expect(recipesImages).toContain(file);
         })
     });
 });
