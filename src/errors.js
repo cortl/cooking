cf2767dbee878177a0b3d75b24ec44392edfe0e8
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2022-01-30 21:19:39 -0600

    removing recipe scraping from this repo

--- src/errors.js
+++
@@ -1,9 +0,0 @@
-class NoParserError extends Error {
-  constructor(site, ...params) {
-    super(...params);
-
-    this.message = `Parser does not exist for ${site}`;
-  }
-}
-
-export { NoParserError };
--- src/filters.js
+++
@@ -1,5 +0,0 @@
-const byRecipeHasRating = ({ rating }) => Boolean(rating);
-const byRecipeHasURL = ({ url }) => Boolean(url);
-const byRecipeShouldBeSkipped = ({ skip }) => skip !== "TRUE";
-
-export { byRecipeHasRating, byRecipeHasURL, byRecipeShouldBeSkipped };
