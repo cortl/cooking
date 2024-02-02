9923fb4eef71155f9b7204d34f5706ca24936713
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2024-02-02 10:41:41 -0600

    prettier again

--- src/update-firestore.js
+++ src/update-firestore.js
@@ -1,28 +1,26 @@
-import { Firestore } from '@google-cloud/firestore';
-import fs from 'fs';
+import { Firestore } from "@google-cloud/firestore";
+import fs from "fs";
 
 (async () => {
-    const firestore = new Firestore();
+  const firestore = new Firestore();
 
-    const files = fs.readdirSync("lib");
-    console.log("Reading...");
-    const recipes = files
-        .filter(file => file.endsWith('.json'))
-        .map((file) =>
-            JSON.parse(fs.readFileSync(`lib/${file}`)),
-        );
+  const files = fs.readdirSync("lib");
+  console.log("Reading...");
+  const recipes = files
+    .filter((file) => file.endsWith(".json"))
+    .map((file) => JSON.parse(fs.readFileSync(`lib/${file}`)));
 
-    recipes.forEach(async (recipe) => {
-        try {
-            if (!recipe.slug) {
-                console.log(`Skipping ${recipe.title} as there is no slug set`);
-                return;
-            }
+  recipes.forEach(async (recipe) => {
+    try {
+      if (!recipe.slug) {
+        console.log(`Skipping ${recipe.title} as there is no slug set`);
+        return;
+      }
 
-            const docRef = firestore.collection('recipes').doc(recipe.slug);
-            await docRef.set(recipe);
-        } catch (err) {
-            console.error(err)
-        }
-    })
-})()
+      const docRef = firestore.collection("recipes").doc(recipe.slug);
+      await docRef.set(recipe);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+})();
