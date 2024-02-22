975c42153a987532ee61b23f89f1951247db219f
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2024-02-21 20:44:41 -0600

    trying to publish individual bundle to improve load times

--- src/build.js
+++ src/build.js
@@ -1,11 +1,5 @@
 import fs from "fs";
 
-const generateBuiltString = (asArray, asMap) =>
-  `export default {
-    asArray: ${asArray},
-    asMap: ${asMap}
-}`;
-
 (() => {
   const files = fs.readdirSync("lib");
   console.log("Reading...");
@@ -13,20 +7,8 @@ const generateBuiltString = (asArray, asMap) =>
     JSON.parse(fs.readFileSync(`lib/${file}`)),
   );
 
-  console.log("Building array...");
-  const asArray = JSON.stringify(recipes);
-
-  console.log("Building map...");
-  const asMap = JSON.stringify(
-    recipes.reduce((acc, recipe) => {
-      acc[recipe.slug] = recipe;
-
-      return acc;
-    }, {}),
-  );
-
   console.log("Building bundle...");
-  fs.writeFileSync("index.js", generateBuiltString(asArray, asMap));
+  fs.writeFileSync("index.json", JSON.stringify(recipes));
 
   console.log("Built!");
 })();
