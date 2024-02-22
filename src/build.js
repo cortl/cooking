115c9975269ff76c5e5d11c30f47d9214b75b48c
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2024-02-22 14:59:26 -0600

    prettier

--- src/build.js
+++ src/build.js
@@ -4,20 +4,20 @@ import sharp from "sharp";
 const recipeProcessors = {
   image: async (recipe) => {
     const { image } = recipe;
-    if (!image) return
+    if (!image) return;
 
-    const imageFile = fs.readFileSync(`images/${image}`)
-    const metadata = await sharp(imageFile).metadata()
+    const imageFile = fs.readFileSync(`images/${image}`);
+    const metadata = await sharp(imageFile).metadata();
 
-    if (!metadata.height) return
-    if (!metadata.width) return
+    if (!metadata.height) return;
+    if (!metadata.width) return;
 
     return {
       path: image,
       height: metadata.height,
       width: metadata.width,
-    }
-  }
+    };
+  },
 };
 
 (async () => {
@@ -29,28 +29,33 @@ const recipeProcessors = {
   const files = fs.readdirSync("lib");
   console.log("Reading...");
 
-  console.log('Processing recipes...')
-  const recipes = await Promise.all(files.map(async (file) => {
-    const recipe = JSON.parse(fs.readFileSync(`lib/${file}`))
-    console.log(recipe)
+  console.log("Processing recipes...");
+  const recipes = await Promise.all(
+    files.map(async (file) => {
+      const recipe = JSON.parse(fs.readFileSync(`lib/${file}`));
+      console.log(recipe);
 
-    await Promise.all(Object.keys(recipe).map(async (key) => {
-      if (recipeProcessors[key]) {
-        const processor = recipeProcessors[key]
-        const result = await processor(recipe)
+      await Promise.all(
+        Object.keys(recipe).map(async (key) => {
+          if (recipeProcessors[key]) {
+            const processor = recipeProcessors[key];
+            const result = await processor(recipe);
 
-        if (result) {
-          recipe[key] = result
-        }
-      }
-    }))
+            if (result) {
+              recipe[key] = result;
+            }
+          }
+        }),
+      );
 
-    return recipe
-  }));
+      return recipe;
+    }),
+  );
 
-  console.log('Rewriting recipes...')
+  console.log("Rewriting recipes...");
   recipes.forEach((recipe) =>
-    fs.writeFileSync(`dist/${recipe.slug}.json`, JSON.stringify(recipe)))
+    fs.writeFileSync(`dist/${recipe.slug}.json`, JSON.stringify(recipe)),
+  );
 
   console.log("Building bundle...");
   fs.writeFileSync("dist/index.json", JSON.stringify(recipes));
