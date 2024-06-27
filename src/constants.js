816f20c5475f0c3858ed1e63592076a5dd415229
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2024-06-27 10:31:53 -0500

    first pass, migration script ✅

---
+++ src/constants.js
@@ -0,0 +1,54 @@
+const TIME_LABELS = [
+    "Prep",
+    "Cook",
+    "Proof",
+    "Bulk Ferment",
+    "Caramelize",
+    "Broil",
+    "Marinate",
+    "Active",
+    "Total",
+    "Inactive",
+];
+
+const TIME_MEASUREMENTS = ["day", "days", "hour", "hours", "minute", "minutes"];
+
+const TAGS = [
+    "Vegan",
+    "Vegetarian",
+    "Poultry",
+    "Fish",
+    "Beef",
+    "Pork",
+    "Drink",
+    "Soup",
+    "Appetizer",
+    "Quick",
+    "Salad",
+    "Sandwich",
+    "Pasta",
+    "Dinner",
+    "Dessert",
+    "Breakfast",
+    "Meal Prep",
+    "Topping",
+    "Thanksgiving",
+    "Christmas",
+    "Camping",
+    "Super Bowl",
+    "Baking",
+    "Roasting",
+    "Frying",
+    "Slow Cooker",
+    "Braising & Stewing",
+    "No Cook",
+    "Stovetop",
+    "Fermenting",
+    "Pressure Cooker",
+    "Seasoning Blend",
+    "Grilling",
+    "WIP",
+];
+
+// TODO: this breaks the tests
+export { TAGS, TIME_LABELS, TIME_MEASUREMENTS }
--- test/utils/recipe-schema.js
+++ test/utils/recipe-schema.js
@@ -1,56 +1,5 @@
 const Joi = require("./joi");
-
-const TIME_LABELS = [
-  "Prep",
-  "Cook",
-  "Proof",
-  "Bulk Ferment",
-  "Caramelize",
-  "Broil",
-  "Marinate",
-  "Active",
-  "Total",
-  "Inactive",
-];
-
-const TIME_MEASUREMENTS = ["day", "days", "hour", "hours", "minute", "minutes"];
-
-const TAGS = [
-  "Vegan",
-  "Vegetarian",
-  "Poultry",
-  "Fish",
-  "Beef",
-  "Pork",
-  "Drink",
-  "Soup",
-  "Appetizer",
-  "Quick",
-  "Salad",
-  "Sandwich",
-  "Pasta",
-  "Dinner",
-  "Dessert",
-  "Breakfast",
-  "Meal Prep",
-  "Topping",
-  "Thanksgiving",
-  "Christmas",
-  "Camping",
-  "Super Bowl",
-  "Baking",
-  "Roasting",
-  "Frying",
-  "Slow Cooker",
-  "Braising & Stewing",
-  "No Cook",
-  "Stovetop",
-  "Fermenting",
-  "Pressure Cooker",
-  "Seasoning Blend",
-  "Grilling",
-  "WIP",
-];
+const { TIME_MEASUREMENTS, TIME_LABELS, TAGS } = require("../../src/constants");
 
 const schema = Joi.object({
   title: Joi.title().required(),
